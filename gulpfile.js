23c5d8c48966da7476d7a4c22b00866607177318
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2025-12-22 15:35:33 +0000

    added: Page banner.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
 // Imports:
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
+// Functions:
 function styles() {
 	return gulp.src('src/styles/**/*.scss')
 		.pipe(sass({ outputStyle: 'compressed' }))
 		.pipe(gulp.dest('./dist/css'));
 }
 
+function images() {
+	return gulp.src('src/images/**/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./dist/images'));
+}
+
 // Exports:
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
-	gulp.watch('src/styles/**/*.scss', gulp.parallel(styles));
+	gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles));
 }
