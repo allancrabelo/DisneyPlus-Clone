1be8284f20d8da7ae22982d965dfbd0c36d6ac11
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2025-12-26 18:21:55 +0000

    config: uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 // Functions:
+function scripts() {
+	return gulp.src('src/scripts/**/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist/js'));
+}
+
 function styles() {
 	return gulp.src('src/styles/**/*.scss')
 		.pipe(sass({ outputStyle: 'compressed' }))
@@ -17,8 +24,9 @@ function images() {
 }
 
 // Exports:
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function() {
 	gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles));
+	gulp.watch('./src/scripts/**/*.js', gulp.parallel(scripts));
 }
